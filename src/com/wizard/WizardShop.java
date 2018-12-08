257d6419016877942e32774c49ad079b48190e0b
Alvar Angerjas <alvar.angerjas@gmail.com>
2018-12-08 13:35:14 +0200

    adding

--- src/com/wizard/WizardShop.java
+++ src/com/wizard/WizardShop.java
@@ -1,5 +1,10 @@
 package com.wizard;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * **Spells n shit**
  * Lacarnum Inflamarae  = 25
@@ -12,4 +17,28 @@ package com.wizard;
  */
 
 public class WizardShop {
+    public static void main(String[] args) {
+
+        HashMap<Integer,String> hashMap = new HashMap<>();
+        hashMap.put(25,"Lacarnum Inflamare");
+        hashMap.put(45,"Lumos Solem");
+        hashMap.put(45,"Everte Statum");
+        hashMap.put(50,"Arania Exumai");
+        hashMap.put(60,"Bombarda");
+        hashMap.put(100,"Avada Kedavra");
+        hashMap.put(25,"Vulnera Sanentur");
+
+        Set set = hashMap.entrySet();
+        Iterator iterator = set.iterator();
+        while(iterator.hasNext()) {
+            Map.Entry mentry = (Map.Entry)iterator.next();
+            System.out.print("key is: "+ mentry.getKey() + " & Value is: ");
+            System.out.println(mentry.getValue());
+
+
+        System.out.println("**     Welcome to Wizard shop     **");
+        System.out.println("Please choose a spell you would like to puy: ");
+        System.out.println(" *");
+    }
+
 }
